0cc2eb06ce4e3ffd936ce0f880602e9832ea68c2
rraulm <59672916+rraulm@users.noreply.github.com>
2020-12-19 13:11:26 -0700

    Added enhancements for LDSBC IT 240 - Project 3.5: DFNB - Predict and Prescribe Future Performance - Data analysis views

---
+++ other_scripts/DataOutputDFNB_p3.sql
@@ -0,0 +1,19 @@
+/*****************************************************************************************************************
+NAME:    DataOutputDFNB_p3
+PURPOSE: Create the output for all views created on project 03
+SUPPORT: Jaussi Consulting LLC
+		 www.jaussiconsulting.net
+		 jon@jaussiconsulting.net
+MODIFICATION LOG:
+Ver      Date        Author        Description
+-----   ----------   -----------   -------------------------------------------------------------------------------
+1.0     12/19/2020   Raul Ribeiro  1. Creation of the Script for output
+ 
+RUNTIME: 
+Approx. 10 min
+NOTES:
+Data OutPut for [dbo].[v_BranchYear]
+******************************************************************************************************************/
+
+SELECT *
+  FROM [dbo].[v_BranchYear];
